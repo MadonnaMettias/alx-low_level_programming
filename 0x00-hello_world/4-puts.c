09680b105a9d1040293305a9d860049bff06eb5b
MadonnaMettias <madonnabartygoher@gmail.com>
2023-03-16 23:51:45 +0000

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-gcc main_1.c
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Puts: print "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char string[] = "Programming is like building a multilingual puzzle";
+
+	puts(string);
+	return (0);
+}

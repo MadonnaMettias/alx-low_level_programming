b93e5f90f53a15b060b71fc793e5f8bbb2967e8e
MadonnaMettias <madonnabartygoher@gmail.com>
2023-03-16 21:13:44 +0000

    0. Preprocessor

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include	<stdio.h>
 
 /**
  * main - Entry point
  *
  * Return: Alawys 0 (Success)
  */
-int main(void)
+int	main(void)
 {
 	printf("Hello\n");
-	return (0);
+	return	(0);
 }

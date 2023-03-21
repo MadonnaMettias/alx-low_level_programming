eb2b4493ffabba22ba9db3163fee732eecc52f78
MadonnaMettias <madonnabartygoher@gmail.com>
2023-03-21 18:01:09 +0000

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z' ; i++)
+	{
+		print_alphabet(i);
+	}
+	return (0);
+}

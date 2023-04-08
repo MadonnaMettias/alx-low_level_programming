556a3cc08d5b302f94c0a2161c543d44b74b3f67
MadonnaMettias <madonnabartygoher@gmail.com>
2023-04-08 15:46:09 +0000

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *@argc: an integer to the number of arguments passed to the program.
+ *@argv: array of strings "arguments" passed to the program.
+ *Return: 0 always sucsuess
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

d6499d275d7cc21fee0fc120c64aebe560884032
MadonnaMettias <madonnabartygoher@gmail.com>
2023-04-12 20:37:29 +0000

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,10 +19,10 @@ int main(int argc, char *argv[])
 		for (i = 1; i < argc; i++)
 		{
 			if (atoi(argv[i]) == 0)
-					{
-						printf("Error\n");
-						return (1);
-					}
+			{
+				printf("Error\n");
+				return (1);
+			}
 			else
 			result += atoi(argv[i]);
 		}

622b9c1e684a2858b133bd0cb35c337e6eb77452
MadonnaMettias <madonnabartygoher@gmail.com>
2023-04-13 15:37:23 +0000

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,15 +22,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
+
 	if (n < len2)
 	{
 		str = malloc(sizeof(char) * (len1 + n + 1));
 		if (str == NULL)
 			return (NULL);
+
 		for (i = 0; s1[i] != '\0'; i++)
 			str[i] = s1[i];
 		for (i = 0; i < n; i++)
 			str[len1 + i] = s2[i];
+
 		str[len1 + i] = '\0';
 		return (str);
 	}
@@ -39,10 +42,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str = malloc(sizeof(char) * (len1 + len2 + 1));
 		if (str == NULL)
 			return (NULL);
+
 		for (i = 0; s1[i] != '\0'; i++)
 			str[i] = s1[i];
 		for (i = 0; s2[i] != '\0'; i++)
 			str[len1 + i] = s2[i];
+
 		str[len1 + i] = '\0';
 		return (str);
 	}

2a046e904b89f032c9830e7d08ef709f4b2fe319
MadonnaMettias <madonnabartygoher@gmail.com>
2023-04-13 21:39:36 +0000

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,9 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	*arr = 0;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	if (arr == NULL)
+	if (nmemb == 0 || size == 0 || arr == NULL)
 		return (NULL);
 	return (arr);
 }
